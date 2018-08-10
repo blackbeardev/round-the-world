7aff8b210b71993771601e2c128917987bf9cfb4
flyingfoxweb <erin@flyingfox.net.nz>
2018-08-10 01:35:42 +0000

    Show and new ejs pages created and new get and post routes and new show routes added to app.js file.

--- app.js
+++ app.js
@@ -1,13 +1,87 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
+mongoose.connect("mongodb://localhost/roundtheworld");
+
+// Schema Setup
+
+var destinationSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Destination = mongoose.model("Destination", destinationSchema);
+
+// Destination.create({
+//     name: "Rome, Italy",
+//     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCLt21VklKaQLufaMRjJ8ltUyf5GLofYUk8psLqDi7jPuvVLwOag",
+//     description: "Rome is beautiful in summer, but very crowded with tourists."
+// }, function(err, destination) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("Newly created destination..");
+//         console.log(destination);
+//     }
+// });
+
+
+// Routes
+
 app.get("/", function(req, res) {
     res.render("landing");
 });
+
+app.get("/destinations", function(req, res) {
+    // res.render("destinations", {destinations: destinations});
+    Destination.find({}, function(err, allDestinations) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("index", {destinations: allDestinations});
+        }
+    });
+});
+
+app.get("/destinations/new", function(req, res) {
+    res.render("new");
+});
+
+app.post("/destinations", function(req, res) {
+   var name = req.body.name;
+   var image = req.body.image;
+   var description = req.body.description;
+   var newDestination = {name: name, image: image, description: description};
+   
+   Destination.create(newDestination, function(err, newlyCreated) {
+       if(err) {
+           console.log(err);
+           res.redirect("/destinations/new");
+       } else {
+           console.log("New destination created...");
+           res.redirect("/destinations");
+       }
+   });
+});
+
+
+
+app.get("/destinations/:id", function(req, res) {
+    Destination.findById(req.params.id, function(err, foundDestination) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {destination:foundDestination});
+        }
+    });
+});
+
 
 
 //Start the server:
